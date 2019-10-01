3fb2126e898d7c6c89ca9e6803baf4d5ac2ee34e
Sydney Crumley <srsimps19@gmail.com>
2019-10-01 16:28:28 -0500

    changed code a bit

--- data/data-router.js
+++ data/data-router.js
@@ -4,15 +4,13 @@ const Db = require('./db.js')
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const posts = await Db.find(req.query);
-        res.status(200).json(posts);
-      } catch (e) {
-        console.log(e);
-        // log error to database
-        res
-          .status(500)
-          .json({ message: `Internal Server Error: cannot retrieve database` });
-      }
+router.get('/', (req, res) => {
+    Db
+        .find()
+        .then(post => {
+            res.status(200).json(post)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        });
 });
